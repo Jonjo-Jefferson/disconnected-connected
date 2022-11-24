a504ca73b388c5e8794a9a69711b72bdb9d39473
Jonjo Jefferson <jonjo.jefferson@gmail.com>
2022-11-24 04:23:34 +0000

    top margin fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head';
 const Home: NextPage = () => {
   return (
     <div className="grid lg:items-center justify-center h-screen bg-hero bg-repeat bg-center bg-contain lg:bg-cover lg:bg-no-repeat">
-      <div className="text-center mt-72">
+      <div className="text-center mt-40">
         <h1 className="text-1rem lg:text-15em">
           <span className="text-red-800">disconnecteD</span>:
           <span className="text-blue-800 opacity-40">detcennoC</span>
