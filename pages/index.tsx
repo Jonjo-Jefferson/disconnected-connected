8081747bc23999d7f17eb0969eb591b915df4799
Jonjo Jefferson <jonjo.jefferson@gmail.com>
2022-11-24 04:27:45 +0000

    fixed lg text size

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const Home: NextPage = () => {
   return (
     <div className="grid lg:items-center justify-center h-screen bg-hero bg-repeat bg-center bg-contain lg:bg-cover lg:bg-no-repeat">
       <div className="text-center mt-52">
-        <h1 className="text-1rem lg:text-15em">
+        <h1 className="text-1rem lg:text-5xl">
           <span className="text-red-800">disconnecteD</span>:
           <span className="text-blue-800 opacity-30">detcennoC</span>
         </h1>
